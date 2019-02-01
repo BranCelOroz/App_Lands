7c0d60f29cead8d09934f5ec8e0adec9c870f89b
brandon celeita <bceleita7@outlook.com>
2019-02-01 16:30:28 -0500

    Add project files.

---
+++ Lands/Lands/Views/LandTabbedPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+namespace lands.Views
+{
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LandTabbedPage : TabbedPage
+	{
+		public LandTabbedPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ Lands/Lands/Views/LandsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace lands.Views
+{
+    using Xamarin.Forms;
+   
+	public partial class LandsPage : ContentPage
+	{
+		public LandsPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
